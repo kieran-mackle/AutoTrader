7c0d9a6b2d8e3699752e5a134c72699bd2e574ab
kieran-mackle <60687606+kieran-mackle@users.noreply.github.com>
2021-07-13 20:49:02 +1000

    Delete docs directory

--- docs/_site/app.js
+++
@@ -1,5 +0,0 @@
-/* Registering Service Worker */
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/autotrader/sw.js');
-};
-
--- docs/_tabs/about.md
+++
@@ -1,6 +0,0 @@
----
-title: About
-icon: fas fa-info
-order: 2
----
-This is a test about page. Will it fix anything? Probably not.
