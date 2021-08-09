82f6eab5bb12d4fc6fd633d522f48d5334828cee
Kieran <kemackle98@gmail.com>
2021-08-09 20:26:37 +1000

    fix: updated setup files to relfect each other

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
       name='autotrader',
-      version='0.0.12',
+      version='0.0.13',
       author='Kieran Mackle',
       author_email='kemackle98@gmail.com',
       packages=find_packages(),
-      url='https://pypi.org/project/autotrader/',
-      license='LICENSE',
+      url='https://kieran-mackle.github.io/AutoTrader/',
+      project_urls={'Bug Tracker': 'https://github.com/kieran-mackle/AutoTrader/issues',
+      }
+      license='GPLv3',
       description='A Python-based platform for developing, optimising and deploying automated trading systems.',
       long_description=open('README.md').read(),
       install_requires=[
