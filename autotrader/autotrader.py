c09660aea70b8577b5d8a7b7ee266bf9845a48f9
Kieran Mackle <kemackle98@gmail.com>
2021-08-22 08:00:10 +1000

    docs: updated manifest

--- autotrader/autotrader.py
+++ autotrader/autotrader.py
@@ -11,7 +11,7 @@
 ---------------------------------------------------------------------------
      A Python-Based Development Platform For Automated Trading Systems
                              Kieran Mackle
-                             Version 0.2.1
+                             Version 0.2.2
                              
 """
 
