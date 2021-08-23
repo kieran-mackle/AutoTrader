14b1e373ecf96c843545164f46a4cee9defc7f59
Kieran <kemackle98@gmail.com>
2021-08-24 08:51:53 +1000

    docs: updated help printouts

--- autotrader/lib/printout.py
+++ autotrader/lib/printout.py
@@ -22,26 +22,25 @@ def usage():
 
     print("--------------------------------------------------------------" \
           + "---------------")
-    print("Flag                                 Comment [short flag]")
+    print("Attribute                           Comment")
     print("--------------------------------------------------------------" \
           + "---------------")
     print("Required:")
-    print("  --config <path>                    path to config file [-c]")
+    print("  config (str)                       filename of config file")
     print("\nOptional:")
-    print("  --help                             show help for usage [-h]")
-    print("  --verbosity <int>                  set verbosity (0,1,2) [-v]")
-    print("  --backtest                         run in backtesting mode [-b]")
-    print("  --plot                             plot results of backtest [-p]")
-    print("  --notify <int>                     notify by email when ordering [-n]")
-    print("  --log                              log backtest results to file [-l]")
-    print("  --analyse                          run correlation study of indicators [-a]")
-    print("  --scan                             run in scan mode only [-s]")
-    print("  --optimise                         optimise strategy parameters [-o]")
-    print("  --instruments                      specify specific instruments [-i]")
-    print("  --data                             load custom price data file [-d]")
+    print("  help                               show help for usage")
+    print("  verbosity (int)                    set verbosity (0,1,2)")
+    print("  backtest                           run in backtesting mode")
+    print("  plot                               plot results of backtest")
+    print("  notify (int)                       notify by email when ordering")
+    print("  log                                log backtest results to file")
+    print("  analyse                            run correlation study of indicators")
+    print("  scan                               run in scan mode only")
+    print("  optimise                           optimise strategy parameters")
+    print("  instruments                        specify specific instruments")
+    print("  data                               load custom price data file")
     print("")
-    print("For more information, try using -h <Option>. For example, use ")
-    print(" -h backtest or -h b for more information on the backtesting flag.\n")
+
 
 def option_help(option):
     ''' Print usage instructions. '''
@@ -49,7 +48,7 @@ def option_help(option):
     banner = pyfiglet.figlet_format("AUTOTRADER")
     print(banner)
     if option == 'config' or option == 'c':
-        print("Help for '--config' (-c) option:")
+        print("Help for 'config' option:")
         print("-----------------------------------")
         print("A configuration file must be specified to run AutoTrader. The")
         print("file must be written as a .yaml file according to the template")
@@ -58,12 +57,10 @@ def option_help(option):
         print("Note that the file extension should not be included. The full")
         print("file path does not need to be specified, AutoTrader will search")
         print("for the file in the config/ directory.")
-        
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file")
+
         
     elif option == 'verbosity' or option == 'v':
-        print("Help for '--verbosity' (-v) option:")
+        print("Help for 'verbosity' option:")
         print("-----------------------------------")
         print("The verbosity flag is used to set the level of output")
         print("displayed by the code. A verbosity of zero supresses all")
@@ -72,35 +69,29 @@ def option_help(option):
         print("Verbosity settings will not affect error output.")
         
         print("\nDefault value: 0")
-        
-        print("\nExample usage:")
-        print("./AutoTrader.py -c ConfigFile.yaml -v 1")
+
         
     elif option == 'backtest' or option == 'b':
-        print("Help for '--backtest' (-b) option:")
+        print("Help for 'backtest' option:")
         print("-----------------------------------")
         print("The backtest flag is used to run the strategy in backtest")
         print("mode.")
         
         print("\nDefault value: False")
-        
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file -b")
+ 
         
     elif option == 'plot' or option == 'p':
-        print("Help for '--plot' (-p) option:")
+        print("Help for 'plot' option:")
         print("-----------------------------------")
         print("The plot option is used to create a plot of the price chart")
         print("and strategy-specific indicators. It may be used for both")
         print("livetrading and backtesting.")
         
         print("\nDefault value: False")
-        
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file -b -p")
+
         
     elif option == 'notify' or option == 'n':
-        print("Help for '--notify' (-n) option:")
+        print("Help for 'notify' option:")
         print("-----------------------------------")
         print("The notify option may be used to enable email notifications")
         print("of livetrade activity and AutoScan results.")
@@ -114,12 +105,10 @@ def option_help(option):
         print("be employed in another scheduled job to send the summary.")
         
         print("\nDefault value: 0")
-        
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file -n 1")
+
         
     elif option == 'log' or option == 'l':
-        print("Help for '--log' (-l) option:")
+        print("Help for 'log' option:")
         print("-----------------------------------")
         print("The log option allows logging of backtest results to a")
         print("logfile. The log file will be written to logfiles/ and")
@@ -128,22 +117,18 @@ def option_help(option):
         print("embeded in the log file for future reference.")
         
         print("\nDefault value: False")
-        
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file -b -l")
+
         
     elif option == 'analyse' or option == 'a':
-        print("Help for '--analyse' (-a) option:")
+        print("Help for 'analyse' option:")
         print("-----------------------------------")
         print("Analyser. More information coming soon.")
         
         print("\nDefault value: False")
-        
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file -b -a")
+
         
     elif option == 'scan' or option == 's':
-        print("Help for '--scan' (-s) option:")
+        print("Help for 'scan' option:")
         print("-----------------------------------")
         print("Automated market scanner. When running AutoTrader in this mode,")
         print("the market will be scanned for entry conditions based on the")
@@ -161,23 +146,19 @@ def option_help(option):
         
         print("\nDefault value: False")
 
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file -s major -n -v 1")
         
     elif option == 'optimise' or option == 'o':
-        print("Help for '--optimise' (-o) option:")
+        print("Help for 'optimise' option:")
         print("-----------------------------------")
         print("When this flag is included, AutoTrader will return a dictionary")
         print("containing backtest results, to be used by the optimiser.")
         print("This option is to be used internally by AutoOptimise.")
         
         print("\nDefault value: False")
-        
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file -b -o")
+
     
     elif option == 'data' or option == 'd':
-        print("Help for '--data' (-d) option:")
+        print("Help for 'data' option:")
         print("-----------------------------------")
         print("This flag may be used to specify the filename for custom")
         print("historical price data. Note that if this flag is included,")
@@ -193,19 +174,15 @@ def option_help(option):
         
         print("\nDefault value: None")
         
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file -b -d EUR_USD.csv")
     
     elif option == 'instruments' or option == 'i':
-        print("Help for '--instruments' (-i) option:")
+        print("Help for 'instruments' option:")
         print("-----------------------------------")
         print("This flag may be used to specify instruments to run AutoTrader")
         print("on, overwriting the watchlist in the strategy config file.")
         
         print("\nDefault value: None")
-        
-        print("\nExample usage:")
-        print("./AutoTrader.py -c my_config_file -b -i EUR_USD")
+
         
     elif option == "general":
         print("General help.")
@@ -215,4 +192,4 @@ def option_help(option):
         print("Unrecognised flag ({}).".format(option))
     
     if option != "general":
-        print("\n\nFor general help, use -h general.\n")
+        print("\n\nFor general help, pass 'general' to help.\n")
