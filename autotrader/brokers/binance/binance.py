9ccef91d30f33a61e5b49c56475068457c56f302
Kieran <kemackle98@gmail.com>
2021-11-03 12:32:00 +1000

    docs: added starter files for binance integration

---
+++ autotrader/brokers/binance/binance.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+
+Binance API Wrapper
+=================
+
+"""
+
+from binance.client import Client
+# from binance import ThreadedWebsocketManager
+from binance.exceptions import BinanceAPIException, BinanceOrderException
+
+
+
+class Binance():
+    def __init__(self, binance_config, *args, **kwargs):
+        ''' Initiate Binance client. '''
+        
+        API_KEY = binance_config["API_KEY"]
+        SECRET_KEY = binance_config["SECRET_KEY"]
+        self.client = Client(API_KEY, SECRET_KEY)
+        self.client.API_URL = binance_config["API_URL"]
+        
+    
+    def _create_order(self, instrument, order_type, order_side, size, price=None):
+        '''
+        Creates order.
+        '''
+        
+        try:
+            response = self.client.create_order(symbol = instrument,
+                                                side = order_side,
+                                                type = order_type,
+                                                timeInForce = 'GTC',
+                                                quantity = size,
+                                                price = price)
+        
+        except BinanceAPIException as e:
+            # API exception has occured
+            response = e
+            
+        except BinanceOrderException as e:
+            # Order exception has occured
+            response = e
+        
+        return response
+    
+        
+    def get_NAV(self):
+        ''' Returns Net Asset Value of account. '''
+        
+    
+    def get_price(self, instrument, **dummy_inputs):
+        ''' Returns current price (bid+ask) and home conversion factors.'''
+    
+    def get_pending_orders(self, instrument=None):
+        ''' Get all pending orders in the account. '''
+    
+    def cancel_pending_order(self, order_id):
+        ''' Cancels pending order by ID. '''
+    
+    def get_open_positions(self, instrument = None):
+        ''' Gets the current positions open on the account. '''
+        
+    
+    def place_order(self, order_details):
+        '''
+        Parses order_details dict and handles order.
+        '''
+        
+    def place_market_order(self, order_details):
+        ''' Places market order. '''
+        
+    
+    def place_stop_limit_order(self, order_details):
+        '''
+        Places MarketIfTouchedOrder with Oanda.
+        https://developer.oanda.com/rest-live-v20/order-df/
+        '''
+        
+    
+    def place_limit_order(self, order_details):
+        ''' (NOT YET IMPLEMENTED) PLaces limit order. '''
+        
+
+    def get_stop_loss_details(self, order_details):
+        ''' Constructs stop loss details dictionary. '''
+    
+    def get_take_profit_details(self, order_details):
+        ''' Constructs take profit details dictionary. '''
+        
+
+    def get_data(self, pair, period, interval):
+        ''' Gets candlestick price data. '''
+    
+    def get_balance(self):
+        ''' Returns account balance. '''
+        
+    
+    def get_summary(self):
+        ''' Returns account summary. '''
+    
+    def get_position(self, instrument):
+        ''' Gets position from Oanda. '''
+    
+    def close_position(self, instrument, long_units=None, short_units=None,
+                       **dummy_inputs):
+        ''' Closes all open positions on an instrument. '''
+        
+    
+    def get_precision(self, pair):
+        ''' Returns the allowable precision for a given pair '''
+    
+    def check_precision(self, pair, price):
+        ''' Modify a price based on required ordering precision for pair. ''' 
+    
+    def check_response(self, response):
+        ''' Checks API response (currently only for placing orders) '''
+    
