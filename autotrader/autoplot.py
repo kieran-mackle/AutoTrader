547bf60a7739a2ed87b1894425913e3bd1880d65
Kieran <kemackle98@gmail.com>
2021-11-16 09:23:11 +1000

    docs: cleanup

--- autotrader/autoplot.py
+++ autotrader/autoplot.py
@@ -738,7 +738,6 @@ class AutoPlot():
         short_arrows = htdf[htdf.sell != 0]
         
         # Add glyphs
-        ix = 0
         linked_fig.scatter(htdf.index,
                            htdf['atrLow'],
                            size = 3,
