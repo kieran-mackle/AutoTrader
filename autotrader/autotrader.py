7277c0e6cc2e0305271de2b75a835eb7349a3f3c
Kieran Mackle <kemackle98@gmail.com>
2021-11-29 21:46:25 +1000

    feat: printout tidy up

--- autotrader/autotrader.py
+++ autotrader/autotrader.py
@@ -1244,7 +1244,7 @@ class AutoTrader():
         data_lengths = [len(bot.data) for bot in self.bots_deployed]
         
         if min(data_lengths) != np.mean(data_lengths):
-            print("Warning: mismatched data lengths detected. Correcting via row reduction.")
+            print("\nWarning: mismatched data lengths detected. Correcting via row reduction.")
             self._normalise_bot_data()
     
     def _normalise_bot_data(self):
