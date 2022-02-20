1edf6f2d2ab3beb286a53b7c2de668aa9142f2cb
Kieran Mackle <kemackle98@gmail.com>
2022-02-20 19:13:23 +1000

    docs: cleanup

--- autotrader/brokers/interactive_brokers/utils.py
+++ autotrader/brokers/interactive_brokers/utils.py
@@ -31,3 +31,33 @@ class Utils(BrokerUtils):
                   'Message': message}
         
         return output
+    
+    def check_precision(self, pair, price):
+        ''' Modify a price based on required ordering precision for pair. ''' 
+        N               = self.get_precision(pair)
+        corrected_price = round(price, N)
+        
+        return corrected_price
+    
+    
+    def get_precision(self, pair):
+        ''' Returns the allowable precision for a given pair '''
+        
+        response = self.api.account.instruments(accountID = self.ACCOUNT_ID, 
+                                                instruments = pair)
+        
+        precision = response.body['instruments'][0].displayPrecision
+        
+        return precision
+    
+    
+    def check_trade_size(self, pair, units):
+        ''' Checks the requested trade size against the minimum trade size 
+            allowed for the currency pair. '''
+        response = self.api.account.instruments(accountID=self.ACCOUNT_ID, 
+                                                instruments = pair)
+        # minimum_units = response.body['instruments'][0].minimumTradeSize
+        trade_unit_precision = response.body['instruments'][0].tradeUnitsPrecision
+        
+        return round(units, trade_unit_precision)
+    
