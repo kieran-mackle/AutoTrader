b6ed59f7bb3f0ad770141b19b0d397d1ca2ef46b
Kieran Mackle <kemackle98@gmail.com>
2022-02-20 19:10:34 +1000

    docs: moved checK_response method

--- autotrader/brokers/interactive_brokers/utils.py
+++ autotrader/brokers/interactive_brokers/utils.py
@@ -6,3 +6,28 @@ from autotrader.brokers.broker_utils import BrokerUtils
 class Utils(BrokerUtils):
     def __init__(self):
         pass
+    
+        
+    def check_response(self, response):
+        """Checks API response for errors.
+
+        Parameters
+        ----------
+        response : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        output : TYPE
+            DESCRIPTION.
+        """
+        
+        if response.status != 201:
+            message = response.body['errorMessage']
+        else:
+            message = "Success."
+            
+        output = {'Status': response.status, 
+                  'Message': message}
+        
+        return output
