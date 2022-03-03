3cb5fc68b7157ea5d0419be802b4459fb5266d37
Kieran Mackle <kemackle98@gmail.com>
2022-03-03 10:24:47 +1000

    docs: updated

--- templates/newbroker/utils.py
+++ templates/newbroker/utils.py
@@ -3,3 +3,13 @@ from autotrader.brokers.broker_utils import BrokerUtils
 class Utils(BrokerUtils):
     def __init__(self):
         pass
+    
+    
+    def __repr__(self):
+        return 'AutoTrader Broker Utilities'
+    
+    
+    def __str__(self):
+        return 'AutoTrader Broker Utilities'
+    
+    
