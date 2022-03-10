3b08e132a8f9cb4647791fa5b1c2df50730d13d6
Kieran Mackle <kemackle98@gmail.com>
2022-03-10 14:22:36 +1000

    docs: updated python dependency

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     ],
     package_dir={"": "."},
     packages=setuptools.find_packages(where="."),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires = [
         "pandas",
         "pyfiglet",
