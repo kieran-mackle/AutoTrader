e7c9fd18095769eec33b19320ea9322cefebe582
Kieran Mackle <kemackle98@gmail.com>
2022-03-15 08:58:33 +1000

    docs: added more tests

--- tests/test_backtest.py
+++ tests/test_backtest.py
@@ -40,4 +40,47 @@ def test_macd_backtest():
     assert bt_results['long_trades']['no_trades'] == 99, "Incorrect number of long trades"
     assert bt_results['short_trades']['no_trades'] == 137, "Incorrect number of short trades"
 
-test_macd_backtest()
+
+def test_multibot_macd_backtest():
+    config = {'NAME': 'MACD Strategy',
+              'MODULE': 'macd_strategy',
+              'CLASS': 'SimpleMACD',
+              'INTERVAL': 'H4',
+              'PERIOD': 300,
+              'RISK_PC': 1.5,
+              'SIZING': 'risk',
+              'PARAMETERS': {'ema_period': 200,
+                             'MACD_fast': 5,
+                             'MACD_slow': 19,
+                             'MACD_smoothing': 9,
+                             'RR': 1.5},
+              'WATCHLIST': ['EUR_USD', 'EUR_USD2'],}
+    home_dir = os.path.abspath(os.path.dirname(__file__))
+    
+    at = AutoTrader()
+    at.configure(verbosity=2, show_plot=True)
+    at.add_strategy(config_dict=config, strategy=SimpleMACD)
+    at.plot_settings(show_cancelled=False)
+    at.add_data({'EUR_USD': 'EUR_USD_H4.csv',
+                 'EUR_USD2': 'EUR_USD_H4.csv'}, 
+                data_directory=os.path.join(home_dir, 'data'))
+    at.backtest(start = '1/1/2015',
+                end = '1/3/2022',
+                initial_balance=1000,
+                leverage=30,
+                spread=0.5,
+                commission=0.005)
+    at.run()
+    bots = at.get_bots_deployed()
+    EU1bot = bots['EUR_USD']
+    EU2bot = bots['EUR_USD2']
+    bt_results = at.multibot_backtest_results
+    
+    assert round(bt_results['win_rate'][0],5) == 38.29787
+    assert round(bt_results['win_rate'][1],5) == 38.96104
+    assert bt_results['no_long'][0] == 99, "Incorrect number of long trades"
+    assert bt_results['no_long'][1] == 97, "Incorrect number of long trades"
+    assert bt_results['no_short'][0] == 136, "Incorrect number of short trades"
+    assert bt_results['no_short'][1] == 134, "Incorrect number of short trades"
+    assert round(EU1bot.backtest_summary['account_history']['balance'][-1], 5) == 527.94671, "Incorrect ending balance"
+
--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -7,15 +7,37 @@ class TestIndicators:
     def test_supertrend(self):
         testdata = self._get_test_data()
         st = indicators.supertrend(testdata)
-        assert st['trend'][-1] == 1
+        assert st['trend'][-1] == 1, "Supertrend indicator failed"
+    
+    
+    def test_halftrend(self):
+        testdata = self._get_test_data()
+        ht = indicators.half_trend(testdata)
+        assert round(ht['halftrend'][-1], 5) == 1.12344, "Halftrend indicator failed"
+        assert round(ht['atrHigh'][-1], 5) == 1.12593, "Halftrend indicator failed"
+        assert round(ht['atrLow'][-1], 5) == 1.12095, "Halftrend indicator failed"
     
     
     def test_range_filter(self):
         testdata = self._get_test_data()
         rfi = indicators.range_filter(testdata)
-        assert round(rfi['upper'][-1], 6) == 1.12736
-        assert round(rfi['lower'][-1], 6) == 1.119815
-        assert round(rfi['rf'][-1], 6) == 1.123588
+        assert round(rfi['upper'][-1], 6) == 1.12736, "Range filter indicator failed"
+        assert round(rfi['lower'][-1], 6) == 1.119815, "Range filter indicator failed"
+        assert round(rfi['rf'][-1], 6) == 1.123588, "Range filter indicator failed"
+    
+    
+    @staticmethod
+    def test_rolling_signal_list():
+        signals = [0,1,0,0,0,-1,0,0,1,0,0]
+        rolled_signals = indicators.rolling_signal_list(signals)
+        assert rolled_signals == [0, 1, 1, 1, 1, -1, -1, -1, 1, 1, 1], "Rolling signal list indicator failed"
+    
+    
+    @staticmethod
+    def test_unroll_signal_list():
+        signals = [0, 1, 1, 1, 1, -1, -1, -1, 1, 1, 1]
+        unrolled_signals = indicators.unroll_signal_list(signals)
+        assert list(unrolled_signals) == [0,1,0,0,0,-1,0,0,1,0,0], "Unroll signal list indicator failed"
     
     
     @staticmethod
