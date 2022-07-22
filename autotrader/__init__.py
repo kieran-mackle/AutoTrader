c592e1303de9abbe3c26820a1362c19351824178
Kieran <kemackle98@gmail.com>
2022-07-22 11:04:58 +1000

    feat: added broker class to init

--- autotrader/__init__.py
+++ autotrader/__init__.py
@@ -3,3 +3,8 @@ from .autodata import AutoData
 from .autotrader import AutoTrader
 from .utilities import TradeAnalysis, DataStream
 from .brokers.trading import Order, Trade, Position
+from .brokers.ccxt.broker import Broker as CCXTBroker
+from .brokers.dydx.broker import Broker as dYdXBroker
+from .brokers.ib.broker import Broker as IBroker
+from .brokers.oanda.broker import Broker as OandaBroker
+from .brokers.virtual.broker import Broker as VirtualBroker
