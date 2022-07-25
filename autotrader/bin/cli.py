97084a6642b63574bf7a79048d6390527f60f73b
Kieran <kemackle98@gmail.com>
2022-07-25 14:09:16 +1000

    fix: added init file to bin dir

--- autotrader/bin/cli.py
+++ autotrader/bin/cli.py
@@ -1,17 +1,37 @@
+import os
 import click
+import shutil
 
 
 @click.group()
 def cli():
     """AutoTrader command line interface."""
-    click.echo('AutoTrader Command Line Interface coming soon!')
+    pass
 
 
 @click.command()
 def init():
     """Initialises current directory for trading."""
-    pass
-    # click.echo("AutoTrader directory initialised.")
+    # Construct filepaths
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(file_dir, '..', 'data')
+    cwd = os.path.abspath(os.getcwd())
+
+    # Check is config directory exists
+    config_dir = os.path.join(cwd, 'config')
+    if not os.path.isdir(config_dir):
+        os.mkdir(config_dir)
+
+    # Copy global config
+    global_config_fp = os.path.join(data_dir, 'GLOBAL.yaml')
+    shutil.copyfile(global_config_fp, os.path.join(config_dir, 'GLOBAL.yaml'))
+
+    # Check if strategy directory exists
+    strategy_dir = os.path.join(cwd, 'strategies')
+    if not os.path.isdir(strategy_dir):
+        os.mkdir(strategy_dir)
+    
+    click.echo("AutoTrader initialisation complete.")
 
 
 cli.add_command(init)
