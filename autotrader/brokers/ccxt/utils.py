e9f7b697fa0d235a5d545ce4df419a5fb2c60ba9
Kieran <kemackle98@gmail.com>
2022-07-29 10:54:29 +1000

    feat: added precision methods to ccxt and dydx utils

--- autotrader/brokers/ccxt/utils.py
+++ autotrader/brokers/ccxt/utils.py
@@ -1,15 +1,39 @@
 from autotrader.brokers.broker_utils import BrokerUtils
 
 class Utils(BrokerUtils):
-    def __init__(self):
-        pass
+    def __init__(self, exchange: str = None):
+        if exchange is not None:
+            self.connect_to_exchange(exchange)
+    
+
+    def connect_to_exchange(self, exchange: str):
+        try:
+            import ccxt
+            self.api = getattr(ccxt, exchange)()
+            self.markets = self.api.load_markets()
+        except ImportError:
+            raise Exception("Please install ccxt to connect "+\
+                            "to CCXT.")
+        # Except network error?
     
     
     def __repr__(self):
-        return 'AutoTrader Broker Utilities'
+        return 'AutoTrader-CCXT Broker Utilities'
     
     
     def __str__(self):
-        return 'AutoTrader Broker Utilities'
+        return 'AutoTrader-CCXT Broker Utilities'
     
     
+    def get_precision(self, instrument, *args, **kwargs):
+        """Returns the precision of the instrument."""
+        market = self._get_market(instrument)
+        precision = market['precision']
+        unified_response = {'size': precision['amount'],
+                            'price': precision['price']}
+        return unified_response
+    
+
+    def _get_market(self, instrument, *args, **kwargs):
+        """Returns the raw get_market response from a CCXT exchange"""
+        return self.markets[instrument]
--- autotrader/brokers/dydx/utils.py
+++ autotrader/brokers/dydx/utils.py
@@ -1,15 +1,36 @@
+from decimal import Decimal
 from autotrader.brokers.broker_utils import BrokerUtils
+
 
 class Utils(BrokerUtils):
     def __init__(self):
-        pass
+        # Create Client instance
+        try:
+            from dydx3 import Client
+            self.api = Client(host='https://api.dydx.exchange')
+        except ImportError:
+            raise Exception("Please install dydx-v3-python to connect "+\
+                            "to dydx API.")
+        # Except network error?
     
     
     def __repr__(self):
-        return 'AutoTrader Broker Utilities'
+        return 'AutoTrader-dYdX Broker Utilities'
     
     
     def __str__(self):
-        return 'AutoTrader Broker Utilities'
+        return 'AutoTrader-dYdX Broker Utilities'
     
     
+    def get_precision(self, instrument, *arg, **kwargs):
+        """Returns the precision of the specified instrument."""
+        market = self._get_market(instrument=instrument)
+        unified_response = {'size': -Decimal(market['tickSize']).as_tuple().exponent,
+                            'price': -Decimal(market['stepSize']).as_tuple().exponent}
+        return unified_response
+
+    
+    def _get_market(self, instrument, *args, **kwargs):
+        """Returns the raw get_market response from dYdX"""
+        response = self.api.public.get_markets(instrument)
+        return response.data['markets'][instrument]
