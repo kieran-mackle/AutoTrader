92be08f77015fc30574c70f77dd213ab4bdae6da
Kieran <kemackle98@gmail.com>
2022-10-21 12:51:00 +1000

    build(setup.py): pinned dependencies and added all option to extra requires

--- setup.py
+++ setup.py
@@ -17,6 +17,28 @@ def get_version(rel_path):
     else:
         raise RuntimeError("Unable to find version string.")
 
+# Define extra dependencies
+dydx_dep = ["dydx-v3-python >= 1.9.0"]
+ccxt_dep = ["ccxt >= 2.0.53"]
+oanda_dep = [
+    "v20 >= 3.0.25.0",
+]
+ib_dep = [
+    "ib_insync >= 0.9.70",
+]
+yfinance_dep = [
+    "yfinance >= 0.1.67",
+]
+dev_dep = [
+    "black >= 22.10.0",
+    "sphinx-copybutton >= 0.5.0",
+    "sphinx-inline-tabs >= 2022.1.2b11",
+    "myst-parser >= 0.18.1",
+    "furo >= 2022.9.29",
+    "sphinx-autobuild >= 2021.3.14",
+    "commitizen >= 2.35.0",
+]
+all_dep = ccxt_dep + oanda_dep + ib_dep + yfinance_dep + dev_dep
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -45,7 +67,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    keywords=["algotrading", "finance"],
+    keywords=["algotrading", "finance", "crypto", "forex", "python"],
     package_dir={"": "."},
     packages=setuptools.find_packages(where="."),
     python_requires=">=3.7",
@@ -59,22 +81,18 @@ setuptools.setup(
         "finta >= 1.3",
         "tqdm>=4.64.0",
         "importlib-resources",
-        "Click",
-        "requests",
+        "click >= 8.1.3",
+        "requests >= 2.28.1",
+        "python-telegram-bot >= 13.14",
     ],
     extras_require={
-        "dydx": ["dydx-v3-python"],
-        "ccxt": ["ccxt"],
-        "oanda": [
-            "v20 >= 3.0.25.0",
-        ],
-        "ib": [
-            "ib_insync >= 0.9.70",
-        ],
-        "yfinance": [
-            "yfinance >= 0.1.67",
-        ],
-        "dev": ["black"],
+        "dydx": dydx_dep,
+        "ccxt": ccxt_dep,
+        "oanda": oanda_dep,
+        "ib": ib_dep,
+        "yfinance": yfinance_dep,
+        "dev": dev_dep,
+        "all": all_dep,
     },
     setup_requires=[
         "setuptools_git",
